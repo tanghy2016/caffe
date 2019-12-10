d40dfef459754bf7754dce8cb9b1ac3066917e37
唐华阳 <tanghuayang@ruiyanai.com>
2019-12-10 11:27:38 +0800

    add sigmoid layer

--- tools/create_prototxt/active_func.py
+++ tools/create_prototxt/active_func.py
@@ -29,6 +29,18 @@ def softmax(name, bottom, top=None, axis=-1):
     return layer, top
 
 
+def sigmoid(name, bottom, top=None):
+    if not top:
+        top = name
+    layer = "layer {\n"
+    layer += "  name: \"" + name + "\"\n"
+    layer += "  type: \"Sigmoid\"\n"
+    layer += "  bottom: \"" + bottom + "\"\n"
+    layer += "  top: \"" + top + "\"\n"
+    layer += "}"
+    return layer, top
+
+
 def test_layer():
     layer, top = relu("relu1", "conv1", "conv1", type="ReLU6")
     print(layer)
